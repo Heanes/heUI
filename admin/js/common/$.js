fc486bef2c45f21d3f92395329e3eb113b16ba2c
Heanes <heanes@163.com>
2016-12-12 17:33:12 +0800

    消息提示方法

--- admin/js/common/$.js
+++ admin/js/common/$.js
@@ -5,13 +5,28 @@
  */
 $(function () {
 
+    /**
+     * @doc 扩展jQuery对象，添加自定义函数
+     * @author Heanes
+     * @time 2016-12-12 17:06:29 周一
+     */
     $.extend({
+
+        /* ------------------------------ 提示消息类 ------------------------------ */
+        // 消息常量
+        messageConstants: {
+            type: {
+                success:    'success',
+                info:       'info',
+                warn:       'error'
+            }
+        },
         /**
          * @doc 全局消息展示
          * @param $content string || object(jQuery) 消息内容
          * @param type string success||error||warn||info消息类型
-         * @param delaySec 展示时间
-         * @param fadeOutDelay 动画延迟时间
+         * @param delaySec 展示时间，单位为毫秒
+         * @param fadeOutDelay 消失动画延迟时间，单位为毫秒
          * @author Heanes
          * @time 2016-12-01 17:55:27 周四
          */
@@ -34,24 +49,75 @@ $(function () {
             $messageContent.append('<p>' + $content + '</p>');
             type = type || 'success';
             switch (type){
-                case 'success':
+                case $.messageConstants.type.success:
                     $message.addClass('message-success');
                     break;
-                case 'info':
+                case $.messageConstants.type.info:
                     $message.addClass('message-info');
                     break;
-                case 'warn':
+                case $.messageConstants.type.warn:
                     $message.addClass('message-warning');
                     break;
-                case 'error':
+                case $.messageConstants.type.error:
                     $message.addClass('message-error');
                     break;
             }
             $messageGlobal.empty().append($messageWrap);
             $messageGlobal.show();
-            delaySec = (delaySec || 3) * 1000;
-            fadeOutDelay = (fadeOutDelay || 1) * 1000;
+            delaySec = delaySec || 3000;
+            fadeOutDelay = fadeOutDelay || 1000;
             $messageGlobal.delay(delaySec).fadeOut(fadeOutDelay);
+        },
+
+        /**
+         * @doc 局部消息展示
+         * @param $target object(jQuery) 放置消息的容器(jQuery对象)
+         * @param $content string || object(jQuery) 消息内容
+         * @param type string success||error||warn||info消息类型
+         * @param delaySec 展示时间
+         * @param fadeOutDelay 消失动画延迟时间，单位为毫秒
+         * @author Heanes
+         * @time 2016-12-01 17:55:27 周四
+         */
+        messageLocal: function messageGlobal($target, $content, type, delaySec, fadeOutDelay){
+            if($target == undefined || $target.length == 0) console.error('Error: the message target is null');
+            var messageWrapDomStr =
+                '<div class="message-wrap">'
+                + '<div class="message">'
+                + '<div class="message-handle">'
+                + '<span class="btn-close">x</span>'
+                + '</div>'
+                + '<div class="message-content">'
+                + '</div>'
+                + '</div>'
+                + '</div>';
+            var $messageWrap = $(messageWrapDomStr);
+            var $message = $messageWrap.find('.message');
+            var $messageContent = $message.find('.message-content');
+            $messageContent.append('<p>' + $content + '</p>');
+            type = type || 'success';
+            switch (type){
+                case $.messageConstants.type.success:
+                    $message.addClass('message-success');
+                    break;
+                case $.messageConstants.type.info:
+                    $message.addClass('message-info');
+                    break;
+                case $.messageConstants.type.warn:
+                    $message.addClass('message-warning');
+                    break;
+                case $.messageConstants.type.error:
+                    $message.addClass('message-error');
+                    break;
+            }
+            $target.empty().append($messageWrap);
+            $target.show();
+            delaySec = delaySec || 3000;
+            fadeOutDelay = fadeOutDelay || 1000;
+            $target.delay(delaySec).fadeOut(fadeOutDelay);
         }
+        /* ------------------------------ 提示消息类 ------------------------------ */
+
+
     });
 });
--- admin/js/common/common.js
+++ admin/js/common/common.js
@@ -5,49 +5,6 @@
  */
 $(function () {
 
-    // 显示消息
-    $('#showMessageSuccess').on('click', function () {
-        $.messageGlobal('恭喜，操作成功~ :)~');
-    });
-    $('#showMessageInfo').on('click', function () {
-        $.messageGlobal('提醒，你还需要做某项操作你还需要做某项操作你还需要做某项操作。', 'info');
-    });
-    $('#showMessageWarn').on('click', function () {
-        $.messageGlobal('警告，有某些情况出现。', 'warn');
-    });
-    $('#showMessageError').on('click', function () {
-        $.messageGlobal('错误，出现了问题。', 'error');
-    });
-
-    /**
-     * @doc 全局消息展示
-     * @param $target object(jQuery) 要展示的地方
-     * @param $content string || $ 消息内容
-     * @param type string success||error||warn||info消息类型
-     * @param delay 展示时间
-     * @author Heanes
-     * @time 2016-12-01 17:55:27 周四
-     */
-    var message = function($target, $content, type, delay){
-        ;
-    };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     /**
      * @doc iframe加载后自适应宽高（不适用于跨域）
