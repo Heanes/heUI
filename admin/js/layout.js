85dedfa65d602df5f85ecc3207b2aa42bdc90cee
Heanes <heanes@163.com>
2016-12-15 19:59:22 +0800

    update

--- admin/js/layout.js
+++ admin/js/layout.js
@@ -26,18 +26,22 @@ $(function () {
     $tabsContainer.on('removed', function (event) {
         tabsAddCount--;
     });
-
-
-    $('.menu-left-block').on('click', '.menu-left-group li a.menu-link', function () {
-        var addResult = addJqxTabFromANode($(this), $tabsContainer, tabsLength + tabsAddCount);
-        addResult ? tabsAddCount++ : null;
-        return false;
-    })
-    .on('click', '.menu-left-group li a.menu-parent', function () {
-        // 带子菜单的节点均不跳转链接
-        return false;
+    var selectedTab = 0;
+    $tabsContainer.on('selected', function (event){
+        selectedTab = event.args.item;
     });
-
+    // 刷新标签
+    $('#refreshTab').on('click', function () {
+        var tabContent = $tabsContainer.jqxTabs('getContentAt', selectedTab);
+        $(tabContent).find('iframe').attr('src', $(this).attr('src'));
+    });
+    // 关闭全部标签
+    $('#closeAllTabs').on('click', function () {
+        var tabsCount = tabsAddCount;
+        for(tabsCount; tabsCount > 0; tabsCount--){
+            $tabsContainer.jqxTabs('removeAt', tabsCount);
+        }
+    });
 
     /**
      * @doc a标签点击后添加新的tab
@@ -66,180 +70,6 @@ $(function () {
         $tabsContainer.jqxTabs('addAt', index, tabTitle, tabContent);
         return true;
     }
-
-
-    /**
-     * @doc 左侧菜单缩进
-     * @author Heanes
-     * @time 2016年08月19日11:59:48
-     */
-    var $leftBlock = $('.left-block');
-    var $menuLeftBlock = $leftBlock.find('.menu-left-block');
-    var lapHandleClick = false;
-    $('.left-menu-lap-handle').on('click', function () {
-        if(lapHandleClick){
-            //$menuLeftFamily.removeClass('lapped');
-            //$('.menu-left-family .menu-left-group > li > a .menu-lap,.menu-left-family .menu-left-group > li > a .menu-text').fadeIn();
-            $menuLeftBlock.removeClass('lapped');
-            $('.menu-left-family .menu-left-group > li > a .menu-lap').fadeIn();
-            $('.center-block').animate({
-                'padding-left': '220px'
-            });
-            $leftBlock.animate({
-                'width': '220px'
-            });
-            lapHandleClick = false;
-        }else{
-            $menuLeftBlock.addClass('lapped');
-            //$menuLeftFamily.addClass('lapped');
-            // 菜单相关隐藏
-            //$('.menu-left-family .menu-left-group > li > a .menu-lap,.menu-left-family .menu-left-group > li > a .menu-text').fadeOut();
-            $('.menu-left-family .menu-left-group > li > a .menu-lap').fadeOut();
-            $('.center-block').animate({
-                'padding-left': '30px'
-            });
-            $leftBlock.animate({
-                'width': '30px'
-            });
-            lapHandleClick = true;
-        }
-    });
-
-
-    /**
-     * @doc 获取节点是否是展开状态
-     * @param menuData
-     * @returns {boolean}
-     * @author Heanes
-     * @time 2016-11-29 20:32:28 周二
-     */
-    var getIsExpanded = function (menuData) {
-        return menuData.state != undefined ?
-            (menuData.state.expanded != undefined && menuData.state.expanded) : false;
-    };
-    
-    var renderTop = function (menuData, level, $target) {
-        var $menuTop = $('.menu-top').empty();
-        var $topMenuUl = $('<ul class="menu-list" id="menuTopUl">');
-        $.each(menuData, function (i, item) {
-            var $topLi = $('<li><a href="javascript:;">' + item.text + '</a></li>');
-            $topMenuUl.append($topLi);
-        });
-        $menuTop.append($topMenuUl);
-    };
-    
-    /**
-     * @doc 渲染左侧菜单
-     * @param $target 目标
-     * @param menuData 菜单数据
-     * @param level 层级
-     * @author Heanes
-     * @time 2016-11-29 16:49:23 周二
-     */
-    var renderMenu = function ($target, menuData, level) {
-        var template = '';
-        var expandedStyle = '';
-        level = level || 1;
-        level++;
-        if(level == 1){
-            renderTop(menuData, level);
-            renderMenu($target, menuData.nodes, level);
-        }
-
-        if(level == 2){
-            template += '<div class="menu-left-family">';
-        }else{
-            expandedStyle = getIsExpanded(menuData) ? '' : 'close';
-        }
-
-        template += '<ul class="menu-left-group ' + expandedStyle + '">';
-        $.each(menuData, function (i, item) {
-            var str = '<li>';
-            var href = item.href == undefined || item.href == '' ? 'javascript:;' : item.href;
-            var hasSubNode = false;
-            var aClass = '';
-            var triangleRight = '';
-            if(item.nodes != undefined && item.nodes.length > 0){
-                hasSubNode = true;
-                aClass = 'menu-parent';
-                triangleRight = '<i class="menu-lap triangle-right" aria-hidden="true"></i>';
-            }else{
-                aClass = 'menu-link';
-            }
-            var aId = item.id;
-            str += '<a href="' + href + '" class="' + aClass + '" data-id="' + aId + '">' + triangleRight;
-            str += '<i class="fa fa-list menu-icon" aria-hidden="true"></i><span class="menu-text">' + item.text + '</span>'
-                + '</a>';
-            if(hasSubNode){
-                str += renderMenu($target, item.nodes, level);
-            }
-            str += '</li>';
-            template += str;
-        });
-        template += '</ul>';
-        if(level == 2) {
-            template += '</div>';
-            $target.append(template);
-            /**
-             * @doc 左侧菜单无限极菜单展开折叠
-             * @author Heanes
-             * @time 2016年08月19日12:09:58
-             */
-            $target.find('.menu-left-group li').each(function (i, item) {
-                if($(item).find('ul.menu-left-group').length > 0){
-                    $(item).find('.menu-parent').first().on('click', function () {
-                        $(this).parent().toggleClass('active');
-                        $(this).toggleClass('active');
-                        $(this).find('.menu-lap').first().toggleClass('triangle-down triangle-right');
-                        $(item).find('ul.menu-left-group').first().toggleClass('close');
-                    });
-                }
-            });
-            // TODO 折叠展开状态写入cookie中，刷新后用js读取一次cookie将折叠展开状态恢复
-        }else{
-            return template;
-        }
-    };
-
-    /**
-     * @doc 点击顶部菜单切换侧边菜单
-     * @author Heanes
-     * @time 2016年08月19日12:09:15
-     */
-    var $menuTopUl = $('#menuTopUl');
-    var $menuTopList = $menuTopUl.find('li');
-    var $menuLeftFamily = $('.menu-left-family');
-    $menuTopList.each(function (i, item) {
-        $(item).on('click', function () {
-            $menuTopList.removeClass('active');
-            $menuLeftFamily.removeClass('active');
-            $($menuLeftFamily[i]).addClass('active');
-            $(this).addClass('active');
-        });
-
-        if($(item).hasClass('active')){
-            $($menuLeftFamily).removeClass('active');
-            $($menuLeftFamily[i]).addClass('active');
-        }
-    });
-
-    $('.menu-top').on('click', '.menu-list li a', function (event) {
-        var $currentTarget = $(event.target);
-        var $li = $currentTarget.parent();
-        var $ul = $li.parent();
-        var $liList = $ul.find('li');
-        $liList.each(function (i, item) {
-            $(item).removeClass('active');
-            if($(item).html() == $li.html()){
-                var index = $(item).index();
-                var $menuLeftFamilyList = $('.menu-left-block').find('.menu-left-family');
-                $menuLeftFamilyList.hide();
-                $menuLeftFamilyList.eq(index).show();
-                $li.addClass('active');
-            }
-        });
-    });
-
     /**
      * @doc 获取菜单数据
      * @author Heanes
@@ -255,8 +85,26 @@ $(function () {
                 iconExpand: 'triangle-down',    // 展开时的图标
                 enableIndentLeft: true,         // 允许向左缩进
                 enableLink: true,               // 开启链接
-                enableTopSwitch: false,          // 开启顶部切换标识
+                enableTopSwitch: true,          // 开启顶部切换标识
                 topSwitcherTarget: '.menu-top', // 开启了顶部切换后，根节点展示在此处(填写jQuery选择器支持的字符)
+                style: {
+                    topActive: {
+                        bgColor: '#346190',     // 顶部切换的激活后背景色 topActive.bgColor
+                        color: '#fff'           // 顶部切换的激活后字体色 topActive.color
+                    },
+                    topHover: {
+                        bgColor: '#346190',     // 侧边树的鼠标浮上背景色 topHover.bgColor
+                        color: '#fff'           // 侧边树的鼠标浮上字体色 topHover.color
+                    },
+                    leftSelected: {
+                        bgColor: '#346190',     // 侧边树的选中后的背景色 leftSelected.bgColor
+                        color: '#fff'           // 侧边树的选中后的字体色 leftSelected.color
+                    },
+                    leftHover: {
+                        bgColor: '#346190',     // 侧边树的鼠标浮上背景色 leftHover.bgColor
+                        color: '#fff'           // 侧边树的鼠标浮上字体色 leftHover.color
+                    }
+                },                              // 样式相关
 
                 onNodeSelected: function (event, node) {
                     var $aNode = $('<a></a>').attr('href', node.href).attr('data-nodeId', node.nodeId)
