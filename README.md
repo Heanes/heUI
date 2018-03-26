464379229e1d0242e987270d5d15ee2b47e11779
Heanes <heanes@163.com>
2018-03-26 13:12:01 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ui.heanes.com
 my personal ui, for kinds of web site element
+## Todo
+1. form module, 增加一个可下拉选择、可直接输入的组件
--- dist/js/heanesUI.js
+++ dist/js/heanesUI.js
@@ -65,7 +65,7 @@
 /******/ 	}
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "e2c90366f90debd741bb"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "57af3d7a8522cf2aeb37"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
 /******/ 	
