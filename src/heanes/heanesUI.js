5bc12b11d539016cb5898fc0e50b455bf8f7ff0c
Heanes <heanes@163.com>
2019-03-31 17:54:12 +0800

    add font-awesome doc link

--- src/heanes/heanesUI.js
+++ src/heanes/heanesUI.js
@@ -3,7 +3,7 @@
  * @author Heanes
  * @time 2018-12-10 18:48:53 周一
  */
-// font-awesome图标4.7.0版
+// font-awesome图标4.7.0版 @see https://fontawesome.com/v4.7.0/
 // import '../../public/static/vendor/fonts/font-awesome/4.7.0/css/font-awesome.min.css';
 // font-awesome图标5.8.1-free-web版 @see https://fontawesome.com/icons
 import '../../public/static/vendor/fonts/font-awesome/5.8.1-free-web/css/all.min.css';
